fba1d106360e64a40ac9611f3981f0eea3d6de0a
Goodiny <in-fo@inbox.ru>
2024-05-06 13:59:51 +0400

    Modify telebot.py for parseHome class

--- telebot/telebot.py
+++ telebot/telebot.py
@@ -1,5 +1,6 @@
 import logging
 from pyrogram import Client, filters
+from parseHome import ParseHome
 
 # Настройка логгирования
 logging.basicConfig(level=logging.INFO)
@@ -15,6 +16,26 @@ app = Client("MyBot", api_id=api_id, api_hash=api_hash)
 def handle_text_message(client, message):
     # Логируем полученное сообщение
     logging.info(f"Received message: {message.text}")
+
+    # Парсим сообщение
+    parse_home = ParseHome()
+    parse_home.parse(message.text)
+
+    with open('source_code.txt', 'r') as sc:
+        text = sc.read()
+        data = []
+        data_item = {}
+        for i, line in enumerate(text.split('\n')):
+            if 'm²' in line:
+                data_item['square'] = line
+            if '$' in line:
+                data_item['price'] = line
+            if '2024' in line or 'час назад' in line:
+                data.append(data_item) if data_item else None
+                data_item = {}
+                data_item['date'] = line
+        print(data)
+        client.send_message(message.chat.id, text)
 
     # Отправляем обратно тоже самоее сообшеение
     message.reply_text(message.chat.id, message.text)
