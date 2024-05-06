cc1f326c2860ed5559b787c7589f30f121ebf551
Goodiny <in-fo@inbox.ru>
2024-05-06 14:04:19 +0400

    Modify telebot for data parse

--- telebot/telebot.py
+++ telebot/telebot.py
@@ -20,22 +20,24 @@ def handle_text_message(client, message):
     # Парсим сообщение
     parse_home = ParseHome()
     parse_home.parse(message.text)
+    print(parse_home.data)
+    client.send_message(message.chat.id, parse_home.data)
 
-    with open('source_code.txt', 'r') as sc:
-        text = sc.read()
-        data = []
-        data_item = {}
-        for i, line in enumerate(text.split('\n')):
-            if 'm²' in line:
-                data_item['square'] = line
-            if '$' in line:
-                data_item['price'] = line
-            if '2024' in line or 'час назад' in line:
-                data.append(data_item) if data_item else None
-                data_item = {}
-                data_item['date'] = line
-        print(data)
-        client.send_message(message.chat.id, text)
+    # with open('source_code.txt', 'r') as sc:
+    #     text = sc.read()
+    #     data = []
+    #     data_item = {}
+    #     for i, line in enumerate(text.split('\n')):
+    #         if 'm²' in line:
+    #             data_item['square'] = line
+    #         if '$' in line:
+    #             data_item['price'] = line
+    #         if '2024' in line or 'час назад' in line:
+    #             data.append(data_item) if data_item else None
+    #             data_item = {}
+    #             data_item['date'] = line
+    #     print(parse_home.data)
+    #     client.send_message(message.chat.id, text)
 
     # Отправляем обратно тоже самоее сообшеение
     message.reply_text(message.chat.id, message.text)
