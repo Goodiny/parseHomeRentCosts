3331960f4aa2adbaad319088a6e34b353863ef78
Goodiny <in-fo@inbox.ru>
2024-05-07 17:43:57 +0400

    Add parse_home: ParseHomeSSGE = None to handle_text_message for abstract parseHome and don't double code print data to str

--- telebot/telebot.py
+++ telebot/telebot.py
@@ -18,6 +18,8 @@ app = Client("MyBot", api_id=api_id, api_hash=api_hash)
 # Обработчик текстовых сообщений
 @app.on_message(filters.text & filters.private)
 def handle_text_message(client, message):
+    parse_home: ParseHomeSSGE = None
+
     if message.text.lower() == "спарсить аренду":
         # Логируем полученное сообщение
         logging.info(f"Received rent home: {message.text}")
@@ -25,9 +27,6 @@ def handle_text_message(client, message):
         # Парсим сообщение
         parse_home = ParseHomeSSGE()
         parse_home.get_source_code(ParseHomeSSGE.RENT_URI)
-        print(parse_home.data)
-
-        client.send_message(message.chat.id, get_str_from_data(parse_home.data))
 
     elif message.text.lower() == "спарсить продажу":
         # Логируем полученное сообщение
@@ -36,17 +35,18 @@ def handle_text_message(client, message):
         # Парсим сообщение
         parse_home = ParseHomeSSGE()
         parse_home.get_source_code(ParseHomeSSGE.SALE_URI)
+
+    # Отправляем обратно тоже самое сообшеение
+    if parse_home and parse_home.data:
         print(parse_home.data)
-
-        client.send_message(message.chat.id, get_str_from_data(parse_home.data))
-
-    # Отправляем обратно тоже самоее сообшеение
-    message.reply_text(message.chat.id, message.text)
+        message.reply_text(f"{message.chat.id}\n\n{get_str_from_data(parse_home.data, ', ')}", message.text)
+    else:
+        message.reply_text(f"{message.chat.id}\n\nНе удалось спарсить данные", message.text)
 
 
-def get_str_from_data(data: list) -> str:
-    return "\n\n".join([", ".join([d["date"], d["title"], d["address"], d["price"], d["area"],
-                                   d.get("price_per_metr", '')]) for d in data])
+def get_str_from_data(data: list, sep: str = ' | ') -> str:
+    return "\n\n".join([sep.join([d["date"], d["title"], d["address"], d["price"], d["area"],
+                                    d.get("price_per_metr", '') ]).rstrip(sep) for d in data])
 
 
 def main():
