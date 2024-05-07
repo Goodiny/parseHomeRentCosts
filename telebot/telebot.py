c285c99380e888d8a400c39c5a72ce3870eeb603
Goodiny <in-fo@inbox.ru>
2024-05-07 14:25:39 +0400

    Modify telebot.py by lines from def and remove unused commented line

--- telebot/telebot.py
+++ telebot/telebot.py
@@ -13,6 +13,7 @@ api_hash = config.API_HASH
 
 app = Client("MyBot", api_id=api_id, api_hash=api_hash)
 
+
 # Обработчик текстовых сообщений
 @app.on_message(filters.text & filters.private)
 def handle_text_message(client, message):
@@ -23,24 +24,8 @@ def handle_text_message(client, message):
     parse_home = ParseHome()
     parse_home.get_source_code()
     print(parse_home.data)
-    client.send_message(message.chat.id, "\n\n".join([", ".join([data["date"], data["title"], data["price"], data["area"]])
-                                                    for data in parse_home.data]))
-
-    # with open('source_code.txt', 'r') as sc:
-    #     text = sc.read()
-    #     data = []
-    #     data_item = {}
-    #     for i, line in enumerate(text.split('\n')):
-    #         if 'm²' in line:
-    #             data_item['square'] = line
-    #         if '$' in line:
-    #             data_item['price'] = line
-    #         if '2024' in line or 'час назад' in line:
-    #             data.append(data_item) if data_item else None
-    #             data_item = {}
-    #             data_item['date'] = line
-    #     print(parse_home.data)
-    #     client.send_message(message.chat.id, text)
+    client.send_message(message.chat.id, "\n\n".join([", ".join([data["date"], data["title"], data["price"],
+                                                                 data["area"]]) for data in parse_home.data]))
 
     # Отправляем обратно тоже самоее сообшеение
     message.reply_text(message.chat.id, message.text)
